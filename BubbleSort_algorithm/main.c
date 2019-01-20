b77d53ddbc7cc54aba012dbfb3098bd0f7d3f6bf
Juan Andonaire <andonaire.juan@gmail.com>
2019-01-19 22:18:23 -0500

    Update main.c

--- BubbleSort_algorithm/main.c
+++ BubbleSort_algorithm/main.c
@@ -2,14 +2,61 @@
 //  main.c
 //  BubbleSort_algorithm
 //
-//  Created by Juan Andonaire on 1/19/19.
-//  Copyright © 2019 Juan Andonaire. All rights reserved.
-//
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+#define MAX 20
+int i, total;
+
+void input(int A[]){
+    printf("Enter total of elements: \n");
+    scanf("%d",&total);
+    
+    printf("Enter values: \n");
+    for(i=0;i<total;i++){
+        scanf("%d",&A[i]);
+    }
+}
+
+void output(int A[]){
+    printf("Values entered: \n");
+    for(i=0;i<total;i++){
+        printf("%d ",A[i]);
+    }
+    printf("\n");
+}
+
+void bubbleSort(int A[]){
+    int j,temp;
+    for(i=0;i<total;i++){
+        for(j=0;j<total-i-1;j++){
+            if(A[j]>A[j+1]){
+                temp = A[j];
+                A[j] = A[j+1];
+                A[j+1] = temp;
+            }
+        }
+    }
+}
+
+void outputSorted(int A[]){
+    printf("Values sorted: \n");
+    for(i=0;i<total;i++){
+        printf("%d ",A[i]);
+    }
+    printf("\n");
+}
+
+int main(){
+    int A[MAX];
+    
+    printf("BubbleSort algorithm\n");
+    input(A);
+    output(A);
+    bubbleSort(A);
+    outputSorted(A);
+    
     return 0;
 }
+
+
